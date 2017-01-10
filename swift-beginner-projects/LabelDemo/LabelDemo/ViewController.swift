d6a9f3ca86d8051977e47a24801c38ebf168be8a
Rajesh Dalsaniya <rd@techcrazy.in>
2017-01-10 15:13:57 +0530

    iOS Swift Beginner Projects
    UILabel Demo Project

---
+++ swift-beginner-projects/LabelDemo/LabelDemo/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  LabelDemo
+//
+//  Created by Rajesh Dalsaniya on 10/01/17.
+//  Copyright © 2017 Rajesh Dalsaniya. All rights reserved.
+//  Website www.rajeshdalsaniya.com
+//
+
+/*
+    A view that displays one or more lines of read-only text, often used in conjunction with controls to describe their intended purpose.
+ */
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: property to reference storyboard label
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    // MARK: Action for changing label text
+    @IBAction func changeLabelTest(_ sender: UIButton) {
+        label.text = "Label Text Changed"
+    }
+
+}
+
