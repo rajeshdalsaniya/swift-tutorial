2a17cae0c5d6cc8f77b0d1469d274f8acd0348b9
Rajesh Dalsaniya <rd@techcrazy.in>
2017-01-29 11:53:00 +0530

    Segue Demo
    Segue done from Storyboard without coding. Prepares for and performs
    the visual transition between two view controllers.

---
+++ swift-beginner-projects/SegueDemo/SegueDemo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SegueDemo
+//
+//  Created by Rajesh Dalsaniya on 29/01/17.
+//  Copyright © 2017 Rajesh Dalsaniya. All rights reserved.
+//  Website www.rajeshdalsaniya.com
+//
+
+// Prepares for and performs the visual transition between two view controllers. 
+// NOTE: In this demo we not using any code to perform transition. Kindly check Main Storyboard for Segue which is done without Code
+// https://developer.apple.com/reference/uikit/uistoryboardsegue
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
